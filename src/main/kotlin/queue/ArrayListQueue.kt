f586fea6e07ec56529fde69a2d96ca381361d745
lenblazy <lenblazy@gmail.com>
2023-05-25 07:58:39 +0300

    Add queue list implementation

---
+++ src/main/kotlin/queue/ArrayListQueue.kt
@@ -0,0 +1,21 @@
+package queue
+
+class ArrayListQueue<T>: Queue<T> {
+
+
+
+    override fun enqueue(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun dequeue(): T? {
+        TODO("Not yet implemented")
+    }
+
+    override val count: Int
+        get() = TODO("Not yet implemented")
+
+    override fun peek(): T? {
+        TODO("Not yet implemented")
+    }
+}
