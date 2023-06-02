a6a7d98011d110f40ee011eda02316f621039294
lenblazy <lenblazy@gmail.com>
2023-06-02 09:36:35 +0300

    Add depth first serach for trees

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import linkedlist.LinkedList
 import queue.ArrayListQueue
 import queue.StackQueue
 import stack.StackImpl
+import tree.TreeNode
 
 fun main(args: Array<String>) {
 //    "creating and linking nodes" example
@@ -52,19 +53,51 @@ fun main(args: Array<String>) {
 //    println(queue)
 //    println("Next up: ${queue.peek()}")
 
-    val queue = StackQueue<String>().apply {
-        enqueue("Ray")
-        enqueue("Brian")
-        enqueue("Eric")
-    }
-    println(queue)
-    queue.dequeue()
-    println(queue)
-    queue.enqueue("Lennox")
-    println(queue)
-    println("Next up: ${queue.peek()}")
-    queue.dequeue()
-    println(queue)
+//    val queue = StackQueue<String>().apply {
+//        enqueue("Ray")
+//        enqueue("Brian")
+//        enqueue("Eric")
+//    }
+//    println(queue)
+//    queue.dequeue()
+//    println(queue)
+//    queue.enqueue("Lennox")
+//    println(queue)
+//    println("Next up: ${queue.peek()}")
+//    queue.dequeue()
+//    println(queue)
+
+    val tree = makeBeverageTree()
+    tree.forEachDepthFirst { println(it.value) }
+}
+
+fun makeBeverageTree(): TreeNode<String> {
+    val tree = TreeNode("Beverages")
+    val hot = TreeNode("hot")
+    val cold = TreeNode("cold")
+    val tea = TreeNode("tea")
+    val coffee = TreeNode("coffee")
+    val chocolate = TreeNode("cocoa")
+    val blackTea = TreeNode("black")
+    val greenTea = TreeNode("green")
+    val chaiTea = TreeNode("chai")
+    val soda = TreeNode("soda")
+    val milk = TreeNode("milk")
+    val gingerAle = TreeNode("ginger ale")
+    val bitterLemon = TreeNode("bitter lemon")
+    tree.add(hot)
+    tree.add(cold)
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+    cold.add(soda)
+    cold.add(milk)
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+    return tree
 }
 
 class Car(var doors: Int)
---
+++ src/main/kotlin/tree/TreeNode.kt
@@ -0,0 +1,16 @@
+package tree
+
+typealias Visitor<T> = (TreeNode<T>) -> Unit
+
+class TreeNode<T>(val value: T) {
+    private val children: MutableList<TreeNode<T>> = mutableListOf()
+
+    fun add(child: TreeNode<T>) = children.add(child)
+
+    fun forEachDepthFirst(visit: Visitor<T>) {
+        visit(this)
+        children.forEach {
+            it.forEachDepthFirst(visit)
+        }
+    }
+}
