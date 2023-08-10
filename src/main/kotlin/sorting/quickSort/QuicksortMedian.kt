344ee64120bd734a9fff42382c5be015858a5836
lenblazy <lenblazy@gmail.com>
2023-08-10 09:50:22 +0300

    Add QuicksortMedian

---
+++ src/main/kotlin/sorting/quickSort/QuicksortMedian.kt
@@ -0,0 +1,33 @@
+package sorting.quickSort
+
+import sorting.swapAt
+
+fun <T : Comparable<T>> MutableList<T>.medianOfThree(
+    low: Int,
+    high: Int
+): Int {
+    val center = (low + high) / 2
+    if (this[low] > this[center]) {
+        this.swapAt(low, center)
+    }
+    if (this[low] > this[high]) {
+        this.swapAt(low, high)
+    }
+    if (this[center] > this[high]) {
+        this.swapAt(center, high)
+    }
+    return center
+}
+
+fun <T : Comparable<T>> MutableList<T>.quickSortMedian(
+    low: Int,
+    high: Int
+) {
+    if (low < high) {
+        val pivotIndex = medianOfThree(low, high)
+        this.swapAt(pivotIndex, high)
+        val pivot = partitionLomuto(low, high)
+        this.quicksortLomuto(low, pivot - 1)
+        this.quicksortLomuto(pivot + 1, high)
+    }
+}
